f5fe2b433c3cc99de64417c6a2374d508daa4325
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-08-12 19:28:36 +0300

    updated filter to controlled component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,12 @@
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import actions from 'redux/phonebook/phonebook-actions';
+import { getFilter } from 'redux/phonebook/phonebook-selectors';
 // import PropTypes from 'prop-types';
 import s from './Filter.module.css';
 
 function Filter() {
+  const filter = useSelector(getFilter);
+
   const dispatch = useDispatch();
   const onFilter = e => dispatch(actions.changeFilter(e.target.value));
 
@@ -19,6 +22,7 @@ function Filter() {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
+        value={filter}
         onChange={onFilter}
       />
     </>
