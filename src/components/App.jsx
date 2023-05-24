92d86b40e2dcb2ef1f680bb30a84792a7f1a7193
Mykyta Kutelmakh <105374551+Reinghord@users.noreply.github.com>
2023-05-24 09:05:24 +0300

    Fix/ deleted obsolete code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { useSelector } from 'react-redux';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 
 function App() {
-  const contacts = useSelector(state => state.contacts);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <div>
       <h1>Phonebook</h1>
